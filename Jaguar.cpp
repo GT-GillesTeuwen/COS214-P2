656116415cf33a9e0ffefcd1a5ae83e1b00ed794
GT-GillesTeuwen <gillesteuwen@gmail.com>
2022-08-14 12:55:31 +0200

    Implemented Jaguar

--- Jaguar.cpp
+++ Jaguar.cpp
@@ -1,6 +1,38 @@
 #include "Jaguar.h"
+#include <iostream>
 
-Jaguar::Jaguar(int hp, string attackMove, int attackDamage, string defensiveMove) {
-	// TODO - implement Jaguar::Jaguar
-	throw "Not yet implemented";
+Jaguar::Jaguar(int hp, std::string attackMove, int attackDamage, std::string defensiveMove):Enemy( hp, attackMove, attackDamage,defensiveMove) {
+	
 }
+
+void Jaguar::celebrate(){
+	std::cout<<"Should have fought harder soldier."<<std::endl;
+ }
+
+ bool Jaguar::getHit(SquadMember *z){
+	std::cout<<"Growls in pain as he is maimed. Jaguar turns around and delivers "<< defensiveMove<<" agains "<< z->getName()<<"."<<std::endl;
+	if (HP-(z->getDamage())<=0)
+	{
+		HP=0;
+		return true;
+	}
+
+	HP-=(z->getDamage());
+	return false;
+	
+ }
+
+ void Jaguar::die(){
+	std::cout<<"Gives one last growl before falling over."<<std::endl;
+ }
+
+ bool Jaguar::hitSquadMember(SquadMember* z){
+	std::cout<<"Jaguar leaps toward the "<< z->getName()<<" and delivers a forceful "<< attackMove<<"."<<std::endl;
+	if (z->takeDamage(attackDamage)==0)
+	{
+		return true;
+	}
+
+	return false;	
+	
+ }
